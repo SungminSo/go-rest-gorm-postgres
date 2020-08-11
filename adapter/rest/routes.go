f5209c165698784b883b533eaf0a9532b5e22cfe
SungminSo <soos196@hanyang.ac.kr>
2020-08-11 10:21:22 +0900

    create api path for admin

--- adapter/rest/routes.go
+++ adapter/rest/routes.go
@@ -18,6 +18,20 @@ func (ps *ProjectService) registerRoutes() {
 		return
 	})
 
+	admins := ps.server.Group("/admins")
+	{
+		// 회원가입
+		admins.POST("/registration")
+		// 로그인
+		admins.POST("/login", )
+		// 사용자 목록 조회
+		admins.GET("/user", )
+		// 사용자 가입 관리 - 승인
+		admins.POST("/user", )
+		// 사용자 가입 관리 - 거절
+		admins.PATCH("/user", )
+	}
+
 	users := ps.server.Group("/users")
 	{
 		// 회원가입
